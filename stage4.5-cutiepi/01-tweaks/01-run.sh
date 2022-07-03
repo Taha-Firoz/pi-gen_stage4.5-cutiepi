9e7d4a7f3ebb0b4761d04c79b158012691c212cc
Mohammad Taha Bin Firoz <mohammdh@live.com>
2022-07-03 14:37:20 +0500

    Removed HDMI disabling

--- stage4.5-cutiepi/01-tweaks/01-run.sh
+++ stage4.5-cutiepi/01-tweaks/01-run.sh
@@ -40,12 +40,12 @@ dkms install -m panel-ilitek-ili9881c -v 1.0 -k 5.15.32-v8+
 EOF
 
 
-# Disables hdmi on rpi
-tar xvf files/vc4-1.0.tgz -C "${ROOTFS_DIR}/"
-on_chroot <<EOF
-dkms add -m vc4/1.0
-dkms install -m vc4/1.0 -k 5.15.32-v8+
-EOF
+# # Disables hdmi on rpi
+# tar xvf files/vc4-1.0.tgz -C "${ROOTFS_DIR}/"
+# on_chroot <<EOF
+# dkms add -m vc4/1.0
+# dkms install -m vc4/1.0 -k 5.15.32-v8+
+# EOF
 
 
 
