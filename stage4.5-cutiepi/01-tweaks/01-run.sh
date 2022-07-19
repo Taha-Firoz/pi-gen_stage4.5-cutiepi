76a00387791106942238648a1ff36f6c968b5873
Mohammad Taha Bin Firoz <mohammdh@live.com>
2022-07-19 21:53:49 +0500

    10-inch build with connman

--- stage4.5-cutiepi/01-tweaks/01-run.sh
+++ stage4.5-cutiepi/01-tweaks/01-run.sh
@@ -18,9 +18,14 @@ install -m 644 files/*.dtbo 			"${ROOTFS_DIR}/boot/overlays/"
 install -m 755 files/cutoff 			"${ROOTFS_DIR}/usr/lib/systemd/system-shutdown/"
 install -m 755 files/cutiepi-mcuproxy 		"${ROOTFS_DIR}/usr/local/bin/"
 
-# Install service file
+# Install shell
 install -m 444 files/firoz.shell.service 			"${ROOTFS_DIR}/etc/systemd/system"
 tar xvpf files/firoz_shell.tar.gz -C "${ROOTFS_DIR}/"
+
+# Install connectivity manager
+install -m 444 files/com.Firoz.Connectivity.Manager.conf 			"${ROOTFS_DIR}/etc/dbus-1/system.d"
+install -m 444 files/firoz.connectivity.manager.service 			"${ROOTFS_DIR}/etc/systemd/system"
+tar xvpf files/connection_manager.tar.gz -C "${ROOTFS_DIR}/"
 
 tar xvpf files/panel-config.tgz -C 		"${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
 tar xvpf files/dconf-config.tgz -C 		"${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
@@ -31,13 +36,13 @@ sed -i 's/console=serial0,115200 //'		"${ROOTFS_DIR}/boot/cmdline.txt"
 sed -i 's/quiet //'				"${ROOTFS_DIR}/boot/cmdline.txt"
 sed -i 's/splash //'				"${ROOTFS_DIR}/boot/cmdline.txt"
 
-# # Uncomment to get 10 inch display drivers loaded
-# tar xvpf files/panel-10inch-ilitek-ili9881c-1.0.tgz -C "${ROOTFS_DIR}/"
-# on_chroot <<EOF
-# dkms add -m panel-ilitek-ili9881c/1.0
-# dkms build -m panel-ilitek-ili9881c -v 1.0 -k 5.15.32-v8+
-# dkms install -m panel-ilitek-ili9881c -v 1.0 -k 5.15.32-v8+
-# EOF
+# Uncomment to get 10 inch display drivers loaded
+tar xvpf files/panel-10inch-ilitek-ili9881c-1.0.tgz -C "${ROOTFS_DIR}/"
+on_chroot <<EOF
+dkms add -m panel-ilitek-ili9881c/1.0
+dkms build -m panel-ilitek-ili9881c -v 1.0 -k 5.15.32-v8+
+dkms install -m panel-ilitek-ili9881c -v 1.0 -k 5.15.32-v8+
+EOF
 
 
 # # Disables hdmi on rpi
@@ -51,4 +56,5 @@ EOF
 
 on_chroot << EOF
 systemctl enable firoz.shell.service
+systemctl enable firoz.connectivity.manager.service
 EOF
