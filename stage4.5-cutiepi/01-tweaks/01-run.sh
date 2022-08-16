ffd096581dc9e0ddff7bc82206dc4c8ff68e3489
Mohammad Taha Bin Firoz <mohammdh@live.com>
2022-08-16 18:53:03 +0500

    build-1

--- stage4.5-cutiepi/01-tweaks/01-run.sh
+++ stage4.5-cutiepi/01-tweaks/01-run.sh
@@ -22,11 +22,6 @@ install -m 755 files/cutiepi-mcuproxy 		"${ROOTFS_DIR}/usr/local/bin/"
 install -m 444 files/firoz.shell.service 			"${ROOTFS_DIR}/etc/systemd/system"
 tar xvpf files/firoz_shell.tar.gz -C "${ROOTFS_DIR}/"
 
-# Install connectivity manager
-install -m 444 files/com.Firoz.Connectivity.Manager.conf 			"${ROOTFS_DIR}/etc/dbus-1/system.d"
-install -m 444 files/firoz.connectivity.manager.service 			"${ROOTFS_DIR}/etc/systemd/system"
-tar xvpf files/connection_manager.tar.gz -C "${ROOTFS_DIR}/"
-
 tar xvpf files/panel-config.tgz -C 		"${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
 tar xvpf files/dconf-config.tgz -C 		"${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
 
@@ -37,15 +32,15 @@ sed -i 's/quiet //'				"${ROOTFS_DIR}/boot/cmdline.txt"
 sed -i 's/splash //'				"${ROOTFS_DIR}/boot/cmdline.txt"
 
 # Uncomment to get 10 inch display drivers loaded
-# tar xvpf files/panel-10inch-ilitek-ili9881c-1.0.tgz -C "${ROOTFS_DIR}/"
-# on_chroot <<EOF
-# dkms add -m panel-ilitek-ili9881c/1.0
-# dkms build -m panel-ilitek-ili9881c -v 1.0 -k 5.15.32-v8+
-# dkms install -m panel-ilitek-ili9881c -v 1.0 -k 5.15.32-v8+
-# EOF
+tar xvpf files/panel-10inch-ilitek-ili9881c-1.0.tgz -C "${ROOTFS_DIR}/"
+on_chroot <<EOF
+dkms add -m panel-ilitek-ili9881c/1.0
+dkms build -m panel-ilitek-ili9881c -v 1.0 -k 5.15.32-v8+
+dkms install -m panel-ilitek-ili9881c -v 1.0 -k 5.15.32-v8+
+EOF
 
 
-# # Disables hdmi on rpi
+# Disables hdmi on rpi
 tar xvf files/vc4-1.0.tgz -C "${ROOTFS_DIR}/"
 on_chroot <<EOF
 dkms add -m vc4/1.0
@@ -53,8 +48,3 @@ dkms install -m vc4/1.0 -k 5.15.32-v8+
 EOF
 
 
-
-on_chroot << EOF
-systemctl enable firoz.shell.service
-systemctl enable firoz.connectivity.manager.service
-EOF
