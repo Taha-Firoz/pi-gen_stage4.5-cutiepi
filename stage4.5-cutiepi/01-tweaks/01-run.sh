d2876e997c12520b741d439894c39684e23a9f83
Mohammad Taha Bin Firoz <mohammdh@live.com>
2022-08-30 16:34:40 +0500

    Build-3 Added new VC-4 driver

--- stage4.5-cutiepi/01-tweaks/01-run.sh
+++ stage4.5-cutiepi/01-tweaks/01-run.sh
@@ -38,8 +38,8 @@ sed -i 's/splash //'				"${ROOTFS_DIR}/boot/cmdline.txt"
 tar xvpf files/panel-10inch-ilitek-ili9881c-1.0.tgz -C "${ROOTFS_DIR}/"
 on_chroot <<EOF
 dkms add -m panel-ilitek-ili9881c/1.0
-dkms build -m panel-ilitek-ili9881c -v 1.0 -k 5.15.32-v8+
-dkms install -m panel-ilitek-ili9881c -v 1.0 -k 5.15.32-v8+
+dkms build -m panel-ilitek-ili9881c -v 1.0 -k 5.15.56-v8+
+dkms install -m panel-ilitek-ili9881c -v 1.0 -k 5.15.56-v8+
 EOF
 
 
@@ -47,7 +47,7 @@ EOF
 tar xvf files/vc4-1.0.tgz -C "${ROOTFS_DIR}/"
 on_chroot <<EOF
 dkms add -m vc4/1.0
-dkms install -m vc4/1.0 -k 5.15.32-v8+
+dkms install -m vc4/1.0 -k 5.15.56-v8+
 EOF
 
 
