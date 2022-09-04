2a461b625e03d8e8026ad12461192552ad698b8c
Mohammad Taha Bin Firoz <mohammdh@live.com>
2022-09-04 21:28:00 +0500

    candidate-1

--- stage4.5-cutiepi/01-tweaks/01-run.sh
+++ stage4.5-cutiepi/01-tweaks/01-run.sh
@@ -43,15 +43,15 @@ sed -i 's/splash //'				"${ROOTFS_DIR}/boot/cmdline.txt"
 tar xvpf files/panel-10inch-ilitek-ili9881c-1.0.tgz -C "${ROOTFS_DIR}/"
 on_chroot <<EOF
 dkms add -m panel-ilitek-ili9881c/1.0
-dkms build -m panel-ilitek-ili9881c -v 1.0 -k 5.15.56-v8+
-dkms install -m panel-ilitek-ili9881c -v 1.0 -k 5.15.56-v8+
+dkms build -m panel-ilitek-ili9881c -v 1.0 -k 5.15.61-v8+
+dkms install -m panel-ilitek-ili9881c -v 1.0 -k 5.15.61-v8+
 EOF
 
 # Disables hdmi on rpi
 tar xvf files/5.15.56-vc4-1.0.tgz -C "${ROOTFS_DIR}/"
 on_chroot <<EOF
 dkms add -m vc4/1.0
-dkms install -m vc4/1.0 -k 5.15.56-v8+
+dkms install -m vc4/1.0 -k 5.15.61-v8+
 EOF
 
 on_chroot << EOF
