87a4921118048c7dac47534de2067d4ee39e7def
Mohammad Taha Bin Firoz <mohammdh@live.com>
2023-01-19 12:44:32 -0500

    revert kernel to 5.15.74

--- stage4.5-cutiepi/01-tweaks/01-run.sh
+++ stage4.5-cutiepi/01-tweaks/01-run.sh
@@ -52,16 +52,15 @@ sed -i 's/splash //'				"${ROOTFS_DIR}/boot/cmdline.txt"
 tar xvpf files/panel-10inch-ilitek-ili9881c-1.0.tgz -C "${ROOTFS_DIR}/"
 on_chroot <<EOF
 dkms add -m panel-ilitek-ili9881c/1.0
-dkms build -m panel-ilitek-ili9881c -v 1.0 -k 5.15.84-v8+
-dkms install -m panel-ilitek-ili9881c -v 1.0 -k 5.15.84-v8+
+dkms build -m panel-ilitek-ili9881c -v 1.0 -k 5.15.74-v8+
+dkms install -m panel-ilitek-ili9881c -v 1.0 -k 5.15.74-v8+
 EOF
 
 # Disables hdmi on rpi
 tar xvf files/vc4-1.0.tgz -C "${ROOTFS_DIR}/"
 on_chroot <<EOF
 dkms add -m vc4/1.0
-dkms install -m vc4/1.0 -k 5.15.84-v8+
-cat "${ROOTFS_DIR}/var/lib/dkms/vc4/1.0/build/make.log"
+dkms install -m vc4/1.0 -k 5.15.74-v8+
 EOF
 
 
